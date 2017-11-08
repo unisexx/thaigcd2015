dae268cc1e27cfb2fd7f42f7b4d81271d206567c
unisexx <unisexx@gmail.com>
2017-11-08 16:21:48 +0700

    fix

--- application/modules/laws/views/inc_index.php
+++ application/modules/laws/views/inc_index.php
@@ -10,12 +10,12 @@
 		<?php foreach($categories as $key => $category): ?>
 		<div id="tabs2-<?php echo $key ?>">
 			<?php foreach($category->law->where("start_date <= date(sysdate()) and (end_date >= date(sysdate()) or end_date = date('0000-00-00')) and status = 'approve'")->limit(2)->get() as $key => $law): ?>
-			<?php if($law->category_id ==11): ?><h3><?php echo lang_decode($law->title) ?></h3>
-			<?php echo lang_decode($law->detail) ?>
-			<?php else: ?>
-			<a href="laws/view/<?php echo $law->id ?>"><h3><?php echo lang_decode($law->title) ?></h3></a>
-			<?php endif; ?>
-			<div class="clear"></div>
+				<?php if($law->category_id ==11): ?><h3><?php echo lang_decode($law->title) ?></h3>
+					<?php echo lang_decode($law->detail) ?>
+				<?php else: ?>
+					<a href="laws/view/<?php echo $law->id ?>"><h3><?php echo lang_decode($law->title) ?></h3></a>
+				<?php endif; ?>
+				<div class="clear"></div>
 			<?php endforeach;?>
 		</div>
 		<?php endforeach;?>
